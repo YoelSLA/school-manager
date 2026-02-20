9930d8116b6f71aa1330ead2df2254f0ea1f4b99
YoelSLA <yoel.ventoso@gmail.com>
2026-02-19 21:48:09 -0300

    cambio en migration

--- backend/src/main/resources/db/migration/V3__rename_tables_to_singular.sql
+++
@@ -1,6 +0,0 @@
--- RENOMBRE DE TABLAS A SINGULAR
-ALTER TABLE cursos RENAME TO curso;
-ALTER TABLE designaciones RENAME TO designacion;
-ALTER TABLE materias RENAME TO materia;
-ALTER TABLE empleados_educativos RENAME TO empleado_educativo;
-ALTER TABLE franjas_horarias RENAME TO franja_horaria;
